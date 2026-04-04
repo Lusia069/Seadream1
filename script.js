a99f30aeda4fce345f3acfb0355e730dce2affdf
fjoa0007 <fjoa0007@red.ujaen.es>
2026-04-04 16:21:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+function mostrarSeccion(nombre) {
+
+  document.getElementById("TituloInicio").style.display = "none";
+  document.getElementById("div1").style.display = "none";
+
+  document.getElementById("TituloRegistro").style.display = "none";
+  document.getElementById("div2").style.display = "none";
+
+  document.getElementById("TituloAcercade").style.display = "none";
+  document.getElementById("div3").style.display = "none";
+
+  document.getElementById("TituloCrearActividad").style.display = "none";
+  document.getElementById("Crear").style.display = "none";
+
+  document.getElementById("TituloListado").style.display = "none";
+  document.getElementById("div4").style.display = "none";
+
+  if (nombre == "inicio") {
+    document.getElementById("TituloInicio").style.display = "block";
+    document.getElementById("div1").style.display = "flex";
+  }
+
+  if (nombre == "registro") {
+    document.getElementById("TituloRegistro").style.display = "block";
+    document.getElementById("div2").style.display = "flex";
+  }
+
+  if (nombre == "acercade") {
+    document.getElementById("TituloAcercade").style.display = "block";
+    document.getElementById("div3").style.display = "flex";
+  }
+
+  if (nombre == "crear") {
+    document.getElementById("TituloCrearActividad").style.display = "block";
+    document.getElementById("Crear").style.display = "block";
+  }
+
+  if (nombre == "listado") {
+    document.getElementById("TituloListado").style.display = "block";
+    document.getElementById("div4").style.display = "flex";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 let actividades = [];
 
 function guardarActividad() {
@@ -32,3 +85,4 @@ function mostrarActividades() {
     lista.appendChild(li);
   });
 }
+*/
