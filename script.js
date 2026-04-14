b018944427888e799173098fe239f7e1c81363de
iaa00040-hash <iaa00040@red.ujaen.es>
2026-04-15 01:26:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+
+/* Autores: Ismael Arroyo Arroyo, Lucía Almudena López Villar, Francisco Jesús Ortega Alcántara
+   Equipo: Equipo Alfa
+   Título del proyecto: Seadream
+   GitHub: https://github.com/Lusia069/SSAATT-2526-FE-Equipoalfa.git
+   Asignatura: Servicios y aplicaciones telemáticas
+   Titulación: Grado en Ingeniería de Tecnologías de Telecomunicación
+   Curso: 2025-2026
+*/
+
 function mostrarSeccion(nombre) {
+
+  actualizarMenuActivo(nombre);
 
   document.getElementById("divInicio").className = "oculto";
   document.getElementById("divRegistro").className = "oculto";
@@ -32,75 +44,170 @@ function mostrarSeccion(nombre) {
   }
 }
 
-function inicio() {
+let autenticado = false;
+
+function inicio(event) {
+  event.preventDefault();
   let nombre = document.getElementById("nombre_inicio").value;
   let contraseña = document.getElementById("contraseña_inicio").value;
   let inicio2 = {
     nombre: nombre,
     contraseña: contraseña
   };
+
+  if (nombre === "admin" && contraseña === "1234") {
+    autenticado = true;
+    alert("Autenticación satisfactoria");
+    mostrarSeccion('acercade');
+    gestionarMenuAdmin(autenticado);
+  } else if (nombre === "usuario" && contraseña === "1234") {
+    autenticado = true;
+    alert("Autenticación satisfactoria");
+    mostrarSeccion('acercade');
+    gestionarMenuUsuario(autenticado);
+  } else {
+    mostrarError("Usuario o clave incorrectos");
+  }
+
 }
 
-function registro() {
-  let nombre = document.getElementById("Nombre_Registro").value;
-  let correo = document.getElementById("Correo").value;
-  let nombre2 = document.getElementById("Nombre_usuario").value;
-  let apellidos = document.getElementById("Apellidos_usuario").value;
-  let contraseña = document.getElementById("Contraseña_usuario").value;
-  let contraseña2 = document.getElementById("Contraseña_verificada").value;
-  let provincia = document.getElementById("Provincia_registro").value;
-  if (contraseña != contraseña2) {
-    alert("Las contraseñas no son iguales");
-  }
-  else {
-    let registro2 = {
-      nombre: nombre,
-      correo: correo,
-      nombre2: nombre2,
-      apellidos: apellidos,
-      contraseña: contraseña,
-      contraseña2: contraseña2,
-      provincia: provincia
+function registro(event) {
+  event.preventDefault();
+  const formulario = event.target;
+  const datos = new FormData(formulario);
+  const pass = datos.get("password");
+  const passCheck = datos.get("password-check");
 
-    };
-  }
+  if (pass != passCheck) {
+    mostrarError("Las contraseñas no son iguales");
+    return;
+  } else {
+    const registro = Object.fromEntries(datos.entries());
+    alert("Registro exitoso");
+    mostrarSeccion('inicio');
+
+  };
 }
 
 
 let actividades = [];
 
-function guardarActividad() {
-  let nombre = document.getElementById("Nombre_actividad").value;
-  let provincia = document.getElementById("Provincia_crear").value;
-  let direccion = document.getElementById("Direccion_crear").value;
-  let fecha = document.getElementById("Fecha_crear").value;
-  let horario = document.getElementById("Horario_crear").value;
+function guardarActividad(event) {
+  event.preventDefault()
+  const formulario = event.target;
+  const datos = new FormData(formulario);
+  const actividad = Object.fromEntries(datos.entries());
 
-  let actividad = {
-    nombre: nombre,
-    provincia: provincia,
-    direccion: direccion,
-    fecha: fecha,
-    horario: horario
-  };
   actividades.push(actividad);
+
+  alert("¡Actividad creada con éxito!");
+
+  formulario.reset();
+  actualizarLista();
 
 }
 
-function modificarActividad() {
-  let nombre = document.getElementById("Nombre_actividadmodificar").value;
-  let provincia = document.getElementById("Provinciamodificar").value;
-  let direccion = document.getElementById("Direccionmodificar").value;
-  let fecha = document.getElementById("Fechamodificar").value;
-  let horario = document.getElementById("Horariomodificar").value;
+function modificarActividad(event) {
+  event.preventDefault()
+  const formulario = event.target;
+  const datos = new FormData(formulario);
+  const actividad_modificada = Object.fromEntries(datos.entries());
 
-  let modificada = {
-    nombre: nombre,
-    provincia: provincia,
-    direccion: direccion,
-    fecha: fecha,
-    horario: horario
-  };
+  actividades.push(actividad_modificada);
+  alert("¡Actividad modificada con éxito!");
 
+  formulario.reset();
+  mostrarSeccion('listado');
+  actualizarLista();
 
+}
+
+function gestionarMenuAdmin(autenticado) {
+  const itemsRestringidos = ["menuCrear", "menuListado"];
+  const itemsNoRestringidos = ["menuInicio", "menuRegistro"];
+
+  itemsRestringidos.forEach(id => {
+    const elemento = document.getElementById(id);
+    if (autenticado) {
+      elemento.classList.remove("oculto");
+      elemento.classList.add("visible");
+    }else{
+      elemento.classList.add("oculto");
+      elemento.classList.remove("visible");
+    }
+  });
+
+  itemsNoRestringidos.forEach(id => {
+    const elemento = document.getElementById(id);
+    if (autenticado) {
+      elemento.classList.add("oculto");
+      elemento.classList.remove("visible");
+    }else{
+      elemento.classList.remove("oculto");
+      elemento.classList.add("visible");
+    }
+  });
+
+}
+
+function gestionarMenuUsuario(autenticado) {
+
+  const itemsNoRestringidos = ["menuInicio", "menuRegistro"];
+  const elemento = document.getElementById('menuListado');
+  const botononesRestringidos = ["botonBorrar", "botonModificar"];
+
+  if (autenticado) {
+    elemento.classList.remove("oculto");
+    elemento.classList.add("visible");
+  }else{
+    elemento.classList.add("oculto");
+    elemento.classList.remove("visible");
+  }
+
+  itemsNoRestringidos.forEach(id => {
+    const elemento = document.getElementById(id);
+    if (autenticado) {
+      elemento.classList.add("oculto");
+      elemento.classList.remove("visible");
+    }else{
+      elemento.classList.remove("oculto");
+      elemento.classList.add("visible");
+    }
+  });
+
+  botononesRestringidos.forEach(id => {
+    const elemento = document.getElementById(id);
+    if (autenticado) {
+      elemento.classList.add("oculto");
+      elemento.classList.remove("visible");
+    }else{
+      elemento.classList.remove("oculto");
+      elemento.classList.add("visible");
+    }
+  });
+
+}
+
+function actualizarMenuActivo(seccion) {
+  const enlaces = document.querySelectorAll('nav ul li a');
+
+  enlaces.forEach(enlace => {
+    enlace.classList.remove('menu-activo');
+
+    if (enlace.getAttribute('onclick').includes(seccion)) {
+      enlace.classList.add('menu-activo');
+    }
+  });
+}
+
+function mostrarError(mensaje) {
+  const error = document.getElementById("mensajeError");
+  error.textContent = mensaje;
+  error.classList.remove("oculto");
+  error.classList.add("visible");
+
+  setTimeout(() => {
+    error.classList.add("oculto");
+    error.classList.remove("visible");
+  }, 5000);
 }
