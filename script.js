7b3772361c11bfeab894595120116ae251487d62
fjoa0007 <fjoa0007@red.ujaen.es>
2026-04-14 19:14:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,47 @@
 function mostrarSeccion(nombre) {
 
-  document.getElementById("TituloInicio").style.display = "none";
-  document.getElementById("div1").style.display = "none";
-
-  document.getElementById("TituloRegistro").style.display = "none";
-  document.getElementById("div2").style.display = "none";
-
-  document.getElementById("TituloAcercade").style.display = "none";
-  document.getElementById("div3").style.display = "none";
-
-  document.getElementById("TituloCrearActividad").style.display = "none";
-  document.getElementById("Crear").style.display = "none";
-
-  document.getElementById("TituloListado").style.display = "none";
-  document.getElementById("div4").style.display = "none";
-
-  document.getElementById("TituloModificar").style.display = "none";
-  document.getElementById("div5").style.display = "none";
-
+  document.getElementById("divInicio").className = "oculto";
+  document.getElementById("divRegistro").className = "oculto";
+  document.getElementById("divCrearActividad").className = "oculto";
+  document.getElementById("divListado").className = "oculto";
+  document.getElementById("divModificarActividad").className = "oculto";
+  document.getElementById("divAcercaDe").className = "oculto";
 
   if (nombre == "inicio") {
-    document.getElementById("TituloInicio").style.display = "block";
-    document.getElementById("div1").style.display = "flex";
+    document.getElementById("divInicio").className = "visible";
   }
 
   if (nombre == "registro") {
-    document.getElementById("TituloRegistro").style.display = "block";
-    document.getElementById("div2").style.display = "flex";
-  }
-
-  if (nombre == "acercade") {
-    document.getElementById("TituloAcercade").style.display = "block";
-    document.getElementById("div3").style.display = "flex";
+    document.getElementById("divRegistro").className = "visible";
   }
 
   if (nombre == "crear") {
-    document.getElementById("TituloCrearActividad").style.display = "block";
-    document.getElementById("Crear").style.display = "block";
+    document.getElementById("divCrearActividad").className = "visible";
   }
 
   if (nombre == "listado") {
-    document.getElementById("TituloListado").style.display = "block";
-    document.getElementById("div4").style.display = "flex";
+    document.getElementById("divListado").className = "visible";
   }
 
-  if (nombre == "modificar"){
-    document.getElementById("TituloModificar").style.display = "block";
-    document.getElementById("div5").style.display = "flex";
+  if (nombre == "modificar") {
+    document.getElementById("divModificarActividad").className = "visible";
+  }
+
+  if (nombre == "acercade") {
+    document.getElementById("divAcercaDe").className = "visible";
   }
 }
 
-function inicio(){
+function inicio() {
   let nombre = document.getElementById("nombre_inicio").value;
-  let contraseña= document.getElementById("contraseña_inicio").value;
-
+  let contraseña = document.getElementById("contraseña_inicio").value;
   let inicio2 = {
-    nombre: nombre, 
+    nombre: nombre,
     contraseña: contraseña
   };
 }
 
-function registro(){
+function registro() {
   let nombre = document.getElementById("Nombre_Registro").value;
   let correo = document.getElementById("Correo").value;
   let nombre2 = document.getElementById("Nombre_usuario").value;
@@ -68,58 +49,58 @@ function registro(){
   let contraseña = document.getElementById("Contraseña_usuario").value;
   let contraseña2 = document.getElementById("Contraseña_verificada").value;
   let provincia = document.getElementById("Provincia_registro").value;
-  if (contraseña != contraseña2){
+  if (contraseña != contraseña2) {
     alert("Las contraseñas no son iguales");
   }
   else {
     let registro2 = {
-    nombre: nombre,
-    correo: correo,
-    nombre2: nombre2,
-    apellidos: apellidos,
-    contraseña: contraseña,
-    contraseña2: contraseña2,
-    provincia: provincia
-    
-  };
+      nombre: nombre,
+      correo: correo,
+      nombre2: nombre2,
+      apellidos: apellidos,
+      contraseña: contraseña,
+      contraseña2: contraseña2,
+      provincia: provincia
+
+    };
   }
 }
 
 
-let actividades=[];
+let actividades = [];
 
-function guardarActividad(){
-    let nombre = document.getElementById("Nombre_actividad").value;
-    let provincia = document.getElementById("Provincia_crear").value;
-    let direccion = document.getElementById("Direccion_crear").value;
-    let fecha = document.getElementById("Fecha_crear").value;
-    let horario = document.getElementById("Horario_crear").value;
+function guardarActividad() {
+  let nombre = document.getElementById("Nombre_actividad").value;
+  let provincia = document.getElementById("Provincia_crear").value;
+  let direccion = document.getElementById("Direccion_crear").value;
+  let fecha = document.getElementById("Fecha_crear").value;
+  let horario = document.getElementById("Horario_crear").value;
 
-    let actividad = {
-        nombre: nombre,
-        provincia: provincia,
-        direccion: direccion,
-        fecha: fecha,
-        horario: horario
-    };
-actividades.push(actividad);
-  
-    }
+  let actividad = {
+    nombre: nombre,
+    provincia: provincia,
+    direccion: direccion,
+    fecha: fecha,
+    horario: horario
+  };
+  actividades.push(actividad);
 
-function modificarActividad(){ 
-      let nombre = document.getElementById("Nombre_actividadmodificar").value;
-      let provincia = document.getElementById("Provinciamodificar").value;
-      let direccion = document.getElementById("Direccionmodificar").value;
-      let fecha = document.getElementById("Fechamodificar").value;
-      let horario = document.getElementById("Horariomodificar").value;
+}
 
-      let modificada = {
-       nombre: nombre,
-       provincia: provincia,
-       direccion: direccion,
-       fecha: fecha,
-       horario: horario
-      };
-      
-      
-    }
+function modificarActividad() {
+  let nombre = document.getElementById("Nombre_actividadmodificar").value;
+  let provincia = document.getElementById("Provinciamodificar").value;
+  let direccion = document.getElementById("Direccionmodificar").value;
+  let fecha = document.getElementById("Fechamodificar").value;
+  let horario = document.getElementById("Horariomodificar").value;
+
+  let modificada = {
+    nombre: nombre,
+    provincia: provincia,
+    direccion: direccion,
+    fecha: fecha,
+    horario: horario
+  };
+
+
+}
